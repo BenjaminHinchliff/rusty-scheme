df92f0c4f44cf5b5d3d2233611c872367a7b7a75
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2022-08-03 12:08:07 +0100

    fix complation problems and restructure types

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, ops, rc::Rc};
+use std::{
+    collections::HashMap,
+    fmt::{self, Display},
+    ops,
+};
 
 use enum_as_inner::EnumAsInner;
 
@@ -8,11 +12,11 @@ use crate::{
     types::SchemeType,
 };
 
-fn option_schemeval_to_string(val: &Option<Rc<SchemeVal>>) -> String {
+fn option_to_string(val: &Option<impl ToString>) -> String {
     val.as_ref().map(|g| g.to_string()).unwrap_or_default()
 }
 
-#[derive(Debug, PartialEq, Eq, thiserror::Error)]
+#[derive(Debug, PartialEq, thiserror::Error)]
 pub enum InterpretError {
     #[error("Parse error: {0}")]
     ParseError(#[from] ParseError),
@@ -30,32 +34,72 @@ pub enum InterpretError {
     ExpectedAtom { given: SchemeVal },
     #[error(
         "Invalid type in call to {function} - given {} but expected {expected}",
-        option_schemeval_to_string(given)
+        option_to_string(given)
     )]
     InvalidType {
         function: String,
-        given: Option<Rc<SchemeVal>>,
+        given: Option<Symbol>,
         expected: SchemeType,
     },
     #[error(
         "Expected function call but was given {}",
-        option_schemeval_to_string(given)
+        option_to_string(given)
     )]
-    ExpectedFunctionCall { given: Option<Rc<SchemeVal>> },
+    ExpectedFunctionCall { given: Option<SchemeVal> },
 }
 
 pub type InterpretResult<T> = Result<T, InterpretError>;
 
-type Value = Rc<SchemeVal>;
+#[derive(Debug, Clone, PartialEq)]
+pub struct Prototype {
+    pub name: String,
+    pub args: Vec<SchemeType>,
+}
 
-#[derive(Clone, EnumAsInner)]
-enum Definition {
-    External(fn(&[&SchemeVal]) -> SchemeVal),
-    Value(Option<Value>),
+#[derive(Clone)]
+pub enum Function {
+    External(Prototype, fn(&[&SchemeVal]) -> SchemeVal),
+    Scheme(Prototype, Box<Symbol>),
+}
+
+impl fmt::Debug for Function {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Function::External(proto, _) => write!(f, "External({:?})", proto),
+            Function::Scheme(proto, body) => write!(f, "Scheme({:?}, {:?})", proto, body),
+        }
+    }
+}
+
+impl PartialEq for Function {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Function::External(s_proto, _), Function::External(o_proto, _)) => s_proto == o_proto,
+            (Function::Scheme(s_proto, s_body), Function::Scheme(o_proto, o_body)) => {
+                s_proto == o_proto && s_body == o_body
+            }
+            _ => false,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, EnumAsInner)]
+pub enum Symbol {
+    Value(SchemeVal),
+    Function(Function),
+}
+
+impl Display for Symbol {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Symbol::Value(v) => write!(f, "{}", v),
+            Symbol::Function(_func) => write!(f, "function"),
+        }
+    }
 }
 
 pub struct Interpreter {
-    symbols: Vec<HashMap<String, Definition>>,
+    variables: Vec<HashMap<String, Option<Symbol>>>,
 }
 
 impl Default for Interpreter {
@@ -67,17 +111,17 @@ impl Default for Interpreter {
 impl Interpreter {
     pub fn new() -> Self {
         Self {
-            symbols: vec![HashMap::new()],
+            variables: vec![HashMap::new()],
         }
     }
 
-    pub fn interpret_str(&mut self, src: &str) -> InterpretResult<Option<Value>> {
+    pub fn interpret_str(&mut self, src: &str) -> InterpretResult<Option<Symbol>> {
         let ast = Parser::new(src).parse()?;
 
         self.interpret(ast)
     }
 
-    pub fn interpret(&mut self, ast: Vec<SchemeVal>) -> InterpretResult<Option<Value>> {
+    pub fn interpret(&mut self, ast: Vec<SchemeVal>) -> InterpretResult<Option<Symbol>> {
         let mut symbol = None;
         for val in ast {
             symbol = self.interpret_one(val)?;
@@ -85,34 +129,31 @@ impl Interpreter {
         Ok(symbol)
     }
 
-    pub fn interpret_one(&mut self, ast: SchemeVal) -> InterpretResult<Option<Value>> {
+    pub fn interpret_one(&mut self, ast: SchemeVal) -> InterpretResult<Option<Symbol>> {
         match ast {
-            s @ SchemeVal::String(_) => Ok(Some(Rc::new(s))),
-            n @ SchemeVal::Number(_) => Ok(Some(Rc::new(n))),
-            b @ SchemeVal::Bool(_) => Ok(Some(Rc::new(b))),
+            s @ SchemeVal::String(_) => Ok(Some(Symbol::Value(s))),
+            n @ SchemeVal::Number(_) => Ok(Some(Symbol::Value(n))),
+            b @ SchemeVal::Bool(_) => Ok(Some(Symbol::Value(b))),
             SchemeVal::List(mut list, tail) => {
                 if let Some(SchemeVal::Atom(atom)) = list.first() {
                     if atom == "quote" {
                         list.remove(0);
-                        return Ok(Some(Rc::new(SchemeVal::List(list, tail))));
+                        return Ok(Some(Symbol::Value(SchemeVal::List(list, tail))));
                     }
                 }
 
                 self.interpret_call(list)
             }
-            SchemeVal::Atom(sym) => self
-                .get_symbol(&sym)
-                .map(|s| s.as_scheme().unwrap()) // TODO: prevent crash when trying to read external symbol
-                .cloned(),
+            SchemeVal::Atom(sym) => self.get_symbol(&sym).cloned(),
         }
     }
 
-    fn add_symbol(&mut self, name: String, definition: Definition) {
-        self.symbols.last_mut().unwrap().insert(name, definition);
+    fn add_symbol(&mut self, name: String, definition: Option<Symbol>) {
+        self.variables.last_mut().unwrap().insert(name, definition);
     }
 
-    fn get_symbol(&self, name: &str) -> InterpretResult<&Definition> {
-        self.symbols
+    fn get_symbol(&self, name: &str) -> InterpretResult<&Option<Symbol>> {
+        self.variables
             .last()
             .unwrap()
             .get(name)
@@ -121,16 +162,14 @@ impl Interpreter {
             })
     }
 
-    fn interpret_call(&mut self, list: Vec<SchemeVal>) -> InterpretResult<Option<Value>> {
+    fn interpret_call(&mut self, list: Vec<SchemeVal>) -> InterpretResult<Option<Symbol>> {
         let mut iter = list.into_iter();
 
         let name = iter
             .next()
             .ok_or(InterpretError::ExpectedFunctionCall { given: None })?
             .into_atom()
-            .map_err(|e| InterpretError::ExpectedFunctionCall {
-                given: Some(Rc::new(e)),
-            })?;
+            .map_err(|e| InterpretError::ExpectedFunctionCall { given: Some(e) })?;
 
         if name == "define" {
             let symbol = iter
@@ -142,15 +181,14 @@ impl Interpreter {
                 })?
                 .into_atom()
                 .map_err(|e| InterpretError::ExpectedAtom { given: e })?;
-            let definition = self.interpret_one(iter.next().ok_or_else(|| {
-                InterpretError::ImproperArgCount {
-                    function: "define".to_string(),
-                    given: 1,
-                    expected: 2,
-                }
-            })?)?;
+            let definition = if let Some(n) = iter.next() {
+                // TODO: find a way to use .map() here?
+                self.interpret_one(n)?
+            } else {
+                None
+            };
 
-            self.add_symbol(symbol, Definition::Value(definition));
+            self.add_symbol(symbol, definition);
             Ok(None)
         } else {
             let op = match name.as_str() {
@@ -164,10 +202,7 @@ impl Interpreter {
             let acc = iter
                 .map(|n| -> InterpretResult<_> {
                     let n = match n {
-                        SchemeVal::Atom(sym) => self
-                            .get_symbol(&sym)
-                            .map(|s| s.as_scheme().unwrap())
-                            .cloned(),
+                        SchemeVal::Atom(sym) => self.get_symbol(&sym).cloned(),
                         n => self.interpret_one(n),
                     }?
                     .ok_or_else(|| InterpretError::InvalidType {
@@ -176,6 +211,12 @@ impl Interpreter {
                         expected: SchemeType::Number,
                     })?;
                     n.clone()
+                        .as_value()
+                        .ok_or_else(|| InterpretError::InvalidType {
+                            function: name.clone(),
+                            given: Some(n.clone()),
+                            expected: SchemeType::Number,
+                        })?
                         .as_number()
                         .ok_or_else(|| InterpretError::InvalidType {
                             function: name.clone(),
@@ -189,7 +230,7 @@ impl Interpreter {
                 .reduce(op)
                 .unwrap();
 
-            Ok(Some(Rc::new(SchemeVal::Number(acc))))
+            Ok(Some(Symbol::Value(SchemeVal::Number(acc))))
         }
     }
 }
@@ -198,9 +239,9 @@ impl Interpreter {
 mod tests {
     use super::*;
 
-    fn check(src: &str, expect: Option<SchemeVal>) {
+    fn check(src: &str, expect: Option<Symbol>) {
         let actual = Interpreter::new().interpret_str(src).unwrap();
-        assert_eq!(actual, expect.map(Rc::new));
+        assert_eq!(actual, expect);
     }
 
     fn check_err(src: &str, err: InterpretError) {
@@ -210,12 +251,15 @@ mod tests {
 
     #[test]
     fn basic_expr() {
-        check("(+ 9 10 2)", Some(SchemeVal::Number(21)));
+        check("(+ 9 10 2)", Some(Symbol::Value(SchemeVal::Number(21))));
     }
 
     #[test]
     fn nested_expr() {
-        check("(* (- 10 8) (+ 10 9))", Some(SchemeVal::Number(38)));
+        check(
+            "(* (- 10 8) (+ 10 9))",
+            Some(Symbol::Value(SchemeVal::Number(38))),
+        );
     }
 
     #[test]
@@ -225,7 +269,7 @@ mod tests {
             two
         "#;
 
-        check(src, Some(SchemeVal::Number(2)));
+        check(src, Some(Symbol::Value(SchemeVal::Number(2))));
     }
 
     #[test]
@@ -244,7 +288,7 @@ mod tests {
         check_err(
             "(2)",
             InterpretError::ExpectedFunctionCall {
-                given: Some(Rc::new(SchemeVal::Number(2))),
+                given: Some(SchemeVal::Number(2)),
             },
         );
     }
